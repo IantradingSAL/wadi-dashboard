cab5ab06ee5bf3f689f84cae0efd9b3e7416dd9d
IantradingSAL <imadaehn@gmail.com>
2026-03-30 01:01:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SUPABASE_URL      = 'https://abcdefgh.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIs...';
+const SUPABASE_URL      = 'هنا الـ URL';https://onjbwhkmmtqnymhjnplw.supabase.co
+const SUPABASE_ANON_KEY = 'هنا الـ anon key';eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uamJ3aGttbXRxbnltaGpucGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ4MDY4MjUsImV4cCI6MjA5MDM4MjgyNX0.lhlsRdOqVHZuOXCJa0lCNuZkYJHhf1AZ_zOwqHHAeG4
